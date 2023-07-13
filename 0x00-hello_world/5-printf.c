a1816b20c7e33b4e6bafa0f59f0e085b23f18d38
AbdallahAg5 <agmarabdallah3@gmail.com>
2023-07-13 13:43:07 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Description: Prints the message
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
