43fa75ad939b6c0d7d3879b440fe06d21e4d45e5
AbdallahAg5 <agmarabdallah3@gmail.com>
2023-07-14 12:37:16 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[24] = "abcdfghijklmnoprstuvwxyz"; /* string with alphabet */
+int i;
+for (i = 23; i >= 0; i--)
+{
+putchar(alp[i]); /* print each char with putchar */
+}
+putchar('\n');
+return (0);
+}
+
