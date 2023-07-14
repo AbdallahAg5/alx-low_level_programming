779fe192193f754826b833bcf5dc07c2b878a98e
AbdallahAg5 <agmarabdallah3@gmail.com>
2023-07-14 09:05:53 -0700

    The last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+srand(time(0)); // random number generator based on the current time
+n = rand() - RAND_MAX / 2; 
 if (n > 0)
 {
 printf("%d is positive\n", n);
